8eb8bad3229097b482c04fe835e10d8dd3dde673
LAN <Lanxinxin2>
2018-07-11 11:20:55 +0800

    更新下

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 module.exports = {
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '123456',
-        database: 'node_blog',
-        port: '3306'
-    }
+	mysql: {
+		host: 'localhost',
+		user: 'root',
+		password: '123456',
+		database: 'node_blog',
+		port: '3306'
+	}
 };
